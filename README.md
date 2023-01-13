762f2b9117aeac18a9332c0966fc0f378024a45d
Digambaran <arjunsreekumar.1@gmail.com>
2023-01-13 16:59:35 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#password_recovery_fn
---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const password_recovery_fn = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello password_recovery_fn`}))
+  res.end()
+  
+}
+
+export default password_recovery_fn
